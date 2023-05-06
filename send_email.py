52f4ad30955ab7cdb82fc948db65e9c35674b84c
muddassarH <muddassarhussain90@gmail.com>
2023-05-07 02:15:08 +0500

    send email via form

--- send_email.py
+++ send_email.py
@@ -1,19 +1,14 @@
 import smtplib
 import ssl
-import pandas
-import streamlit as st
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
-
-
     username = "muddassarhussain90@gmail.com"
-    password = "zgrezriwoezcdbgr"
+    password = os.getenv("PASSWORD")
     receiver_email = "muddassarhussain90@gmail.com"
     context = ssl.create_default_context()
-
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver_email, message)
